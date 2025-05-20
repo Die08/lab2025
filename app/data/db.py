dc851360bdc2e8945b9ec79df1fcdf07f4dff54e
Die08 <03diegocarta@gmail.com>
2025-05-20 13:50:27 +0200

    commenti

--- app/data/db.py
+++ app/data/db.py
@@ -3,32 +3,37 @@ from typing import Annotated
 from fastapi import Depends
 from faker import Faker
 import os
-from models.book import Book
+from app.models.book import Book
 
 
 sqlite_file_name = "app/data/database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args,
-                       echo=True)
+connect_args = {"check_same_thread": False} #isabilita il controllo del thread di SQLite, necessario quando più thread/applicazioni condividono la stessa connessione.
+engine = create_engine( #crea un motore SQLAlchemy/SQLModel puntato al file SQLite app/data/database.db.
+    sqlite_url,
+    connect_args=connect_args,
+                       echo=True) #fa sì che tutte le query SQL generate vengano stampate in console, utile per debug.
 
 
 def init_database():
-    ds_exists = os.path.isfile(sqlite_file_name)
-    SQLModel.metadata.create_all(engine)
+    ds_exists = os.path.isfile(sqlite_file_name) #è un booleano che indica se database.db esiste già sul disco.
+    SQLModel.metadata.create_all(engine) #crea tutte le tabelle definiti nel modello SQLModel.
     if not ds_exists:
-        f = Faker("it_IT")
+        f = Faker("it_IT") #crea un oggetto Faker per generare dati fake.
         with Session(engine) as session:
             for i in range(10):
-                book = Book(title=f.sentence(nb_words=5), author=f.name(),
+                book = Book(title=f.sentence(nb_words=5), author=f.name(), # se il file non esisteva (not ds_exists), viene istanziato Faker("it_IT") per generare 10 record fittizi con titolo (sentence), autore (name) e voto recensione (pyint(1,5)),
                             review=f.pyint(1, 5))
                 session.add(book)
-            session.commit()
+            session.commit() #aggiunti e infine salvati (commit()).
 
 
-def get_session():
+def get_session(): #definisce un generator-based dependency per FastAPI che crea una Session
+    # collegata all’engine, la fornisce al path operation e la chiude automaticamente al termine.
     with Session(engine) as session:
         yield session
 
 
 SessionDep = Annotated[Session, Depends(get_session)]
+#alias di tipo che combina Session con Depends(get_session), semplificando la dichiarazione delle dipendenze
+# nei router FastAPI (ad es. def get_all_books(session: SessionDep)).
--- app/models/book.py
+++ app/models/book.py
@@ -1,6 +1,12 @@
+'''Il file book.py definisce i modelli di dati per la risorsa “Book” utilizzando SQLModel,
+ che unisce le potenzialità di SQLAlchemy (mappatura object-relational) e Pydantic (validazione e serializzazione).'''
 from sqlmodel import SQLModel, Field
 from typing import Annotated
-
+'''QLModel è la classe base da cui ereditano tutti i modelli:
+ fornisce sia il mapping verso il database sia la validazione dei dati.
+Field serve a specificare dettagli aggiuntivi sui campi (es. vincoli, chiavi).
+Annotated (da typing) permette di combinare il tipo Python 
+con metadati (qui usato per la validazione Pydantic) .'''
 
 class BookBase(SQLModel):
     title: str
@@ -8,13 +14,24 @@ class BookBase(SQLModel):
     review: Annotated[int, Field(ge=1, le=5)]
 
 
-class Book(BookBase, table=True):
-    id: int = Field(default=None, primary_key=True)
+class Book(BookBase, table=True): #table=True indica a SQLModel di creare una tabella corrispondente chiamata per convenzione “book”.
+    id: int = Field(default=None, primary_key=True) #Il campo id è dichiarato intero, con primary_key=True
+    # per marcare la chiave primaria; default=None permette al database di assegnare automaticamente un valore (autoincrement)
 
 
 class BookCreate(BookBase):
     pass
+#Viene usato come schema d’ingresso (request body) nelle API quando si crea un nuovo libro;
+# Pydantic controllerà la presenza e la validità dei campi definiti in BookBase bookbook
 
 
 class BookPublic(BookBase):
     id: int
+#Estende BookBase aggiungendo il campo id.
+#Viene impiegato come schema d’uscita (response model) alle API, garantendo che il client riceva anche
+# l’identificativo assegnato dal database
+
+'''BookBase: definisce gli attributi comuni e la logica di validazione.
+Book: mappa la tabella vera e propria, con chiave primaria.
+BookCreate e BookPublic: differenziano gli schemi in ingresso (create) e in uscita (public), 
+garantendo una chiara separazione dei modelli di dominio nei diversi contesti.'''
